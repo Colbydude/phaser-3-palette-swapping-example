19f34ceacd18c3a54ef81fc8c4f9144c67af15da
Colby Terry <colbydude@gmail.com>
2018-03-23 23:03:58 -0700

    Phaser 3 Palette Swapping WIP-4. Working animation building.

--- public/js/palette-swap.js
+++ public/js/palette-swap.js
@@ -37,15 +37,6 @@ function create ()
     // Create the dynamic texture atlases and animations.
     createPalettes(animConfig);
 
-    for (var i = 0; i < animConfig.animations.length; i++) {
-        this.anims.create({
-            key: 'link-default-' + animConfig.animations[i],
-            frames: this.anims.generateFrameNumbers('link', {start: 0 + (i * 10), end: 9 + (i * 10)}),
-            frameRate: 15,
-            repeat: -1
-        });
-    }
-
     console.log(this.textures);
     console.log(this.anims);
 
@@ -54,36 +45,45 @@ function create ()
     this.add.text(5, 20, "X: Change Palette");
 
     // Add sprite.
-    var link = this.add.sprite(120, 80, 'link-green').setScale(2);
+    var link = this.add.sprite(120, 80, 'link').setScale(2);
 
     // Set color and animation.
-    link.color = 'green';
-    link.anims.play('link-' + link.color + '-walk-down');
+    link.color = animConfig.paletteNames[0];
+    link.anims.play('link-' + link.color + '-' + animConfig.animations[0]);
 
     // Handle Input.
     this.input.keyboard.on('keydown_W', function (event) {
         link.flipX = false;
-        link.anims.play('link-' + link.color + '-walk-up');
+        link.anims.play('link-' + link.color + '-' + animConfig.animations[2]);
     });
 
     this.input.keyboard.on('keydown_A', function (event) {
         link.flipX = false;
-        link.anims.play('link-' + link.color + '-walk-left');
+        link.anims.play('link-' + link.color + '-' + animConfig.animations[1]);
     });
 
     this.input.keyboard.on('keydown_S', function (event) {
         link.flipX = false;
-        link.anims.play('link-' + link.color + '-walk-down');
+        link.anims.play('link-' + link.color + '-' + animConfig.animations[0]);
     });
 
     this.input.keyboard.on('keydown_D', function (event) {
         link.flipX = true;
-        link.anims.play('link-' + link.color + '-walk-left');
+        link.anims.play('link-' + link.color + '-' + animConfig.animations[1]);
     });
 
     this.input.keyboard.on('keydown_X', function (event) {
-        // TODO: Palette Swap.
-        console.log('Change palette...');
+        var index = animConfig.paletteNames.indexOf(link.color);
+
+        index++;
+
+        if (index >= animConfig.paletteNames.length) {
+            index = 0;
+        }
+
+        link.color = animConfig.paletteNames[index];
+
+        link.anims.play('link-' + link.color + '-' + animConfig.animations[0]);
     });
 }
 
@@ -111,8 +111,8 @@ function createPalettes (config)
     // Create texture atlas from frame data.
     var animData = {};
     var sheet = game.textures.get(config.spriteSheet).getSourceImage();
-    var atlasData;
-    var atlasKey, animKey, canvasTexture, canvas;
+    var atlasKey, animKey;
+    var canvasTexture, canvas, context;
 
     // Iterate over each palette.
     for (y = 0; y < config.paletteNames.length; y++) {
@@ -121,65 +121,34 @@ function createPalettes (config)
         atlasData = { frames: [] };
 
         // Create a canvas to draw new image data onto.
-        canvasTexture = game.textures.createCanvas(atlasKey, sheet.width, sheet.height);
-        canvas = canvasTexture.getSourceImage().getContext('2d');
+        canvasTexture = game.textures.createCanvas(config.spriteSheet + '-temp', sheet.width, sheet.height);
+        canvas = canvasTexture.getSourceImage();
+        context = canvas.getContext('2d');
+
+        // Copy the sheet.
+        context.drawImage(sheet, 0, 0, sheet.width, sheet.height);
+
+        // TODO: Replace pixels throughout sheet.
+
+        // Add the canvas as a sprite sheet to the game.
+        game.textures.addSpriteSheet(atlasKey, canvasTexture.getSourceImage(), {
+            frameWidth: 32,
+            frameHeight: 32,
+            startFrame: 0,
+            endFrame: 29
+        });
 
         // Iterate over each animation.
         for (var a = 0; a < config.animations.length; a++) {
             animKey = atlasKey + '-' + config.animations[a];
-            animData[animKey] = [];
 
-            // Iterate frames for each animation.
-            // NOTE: Length hard coded for now.
-            for (var f = 0; f < 10; f++) {
-                var frameName = config.animations[a] + (f ? f : '');
-                var frame = game.textures.getFrame(config.spriteSheet, (a * 10) + f);
-
-                // Copy frame into new canvas.
-                canvas.drawImage(
-                    sheet,
-                    frame.cutX, frame.cutY, frame.width, frame.height,
-                    frame.cutX, frame.cutY, frame.width, frame.height
-                );
-                console.log({frame: atlasKey + '-' + frameName, destX: frame.cutX, destY: frame.cutY, dWidth: frame.width, dHeight: frame.height});
-                // NOTE: Locations are seemingly correct, but animation comes out wonky in the end?
-
-                // TODO: Replace RGB in new frame.
-
-                // Add this to the texture atlas definitions.
-                atlasData.frames.push({
-                    filename: atlasKey + '-' + frameName,
-                    frame: { x: frame.cutX, y: frame.cutY, w: frame.width, h: frame.height },
-                    // NOTE: Do I *need* the following?
-                    rotated: false,
-                    trimmed: true,
-                    spriteSourceSize: { x: frame.cutX, y: frame.cutY, w: frame.width, h: frame.height },
-                    sourceSize: { w: frame.width, h: frame.height }
-                });
-
-                // Add this to the animation definitions.
-                animData[animKey].push({
-                    key: atlasKey,
-                    frame: atlasKey + '-' + frameName,
-                    // NOTE: Do I *need* the following?
-                    duration: 0,
-                    visible: false
-                });
-            }
+            // Add the animation to the game.
+            game.anims.create({
+                key: animKey,
+                frames: game.anims.generateFrameNumbers('link', {start: 0 + (a * 10), end: 9 + (a * 10)}),
+                frameRate: 15,  // NOTE: Hard coded for now.
+                repeat: -1
+            });
         }
-
-        // Add texture atlas to game.
-        game.textures.addAtlasJSONArray(atlasKey, canvasTexture.getSourceImage(), atlasData);
-    }
-
-    // Now that the atlases for all the palettes have been made,
-    // make each necessary animation.
-    for (var key in animData) {
-        game.anims.create({
-            key: key,
-            frames: animData[key],
-            frameRate: 15,  // NOTE: Hard coded for now.
-            repeat: -1
-        });
     }
 }
